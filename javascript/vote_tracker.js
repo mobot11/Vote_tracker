653af5d8e68b4e7cf5245db3a48007aca88da90e
Dylan T. Moberg <dylanmoberg@msn.com>
2015-04-15 15:49:36 -0700

    added ajax.js file

--- javascript/vote_tracker.js
+++ javascript/vote_tracker.js
@@ -1,4 +1,46 @@
 $(function(){
+   function runAjax(){
+    var images;
+    var filepath = [];
+    window.filepath;
+
+  var saveToLocalStorage = function(data) {
+    localStorage.setItem('ajaxData', JSON.stringify(data));
+  };
+
+  var renderImages = function(images) {
+    var $main = $('#content');
+
+    images.forEach(function(img) {
+      // $main.append('<img src="' + img.link + '"></img>');
+      filepath.push(img.link);
+      console.log(filepath);
+    });
+    saveToLocalStorage(images);
+  };
+
+
+  $.ajax({
+    url: 'https://api.imgur.com/3/album/GHHvu.json',
+    method: 'GET',
+    headers: {
+      'Authorization': 'Client-ID 771c94be66ed54b'
+    }
+  })
+  .done(function(res) {
+    images = res.data.images
+    renderImages(images);
+  })
+  .fail(function(err) {
+    console.log(err);
+  });
+
+
+};
+
+
+runAjax();
+
 var Kitty = function (imgLink) {
     this.imgLink = imgLink;
     // this.color = color;
@@ -7,10 +49,10 @@ var Kitty = function (imgLink) {
     // kittyArray = [];
 };
 
-var filepath = ["contestants/01.jpg","contestants/02.jpg", "contestants/03.jpg",
-"contestants/04.jpg", "contestants/05.jpg", "contestants/06.jpg", "contestants/07.jpg",
- "contestants/08.jpg", "contestants/09.jpg", "contestants/10.jpg", "contestants/11.jpg",
- "contestants/12.jpg", "contestants/13.jpg","contestants/14.jpg", ];
+// var filepath = ["contestants/01.jpg","contestants/02.jpg", "contestants/03.jpg",
+// "contestants/04.jpg", "contestants/05.jpg", "contestants/06.jpg", "contestants/07.jpg",
+//  "contestants/08.jpg", "contestants/09.jpg", "contestants/10.jpg", "contestants/11.jpg",
+//  "contestants/12.jpg", "contestants/13.jpg","contestants/14.jpg", ];
 
  var kittyArray = [];
 
@@ -41,4 +83,4 @@ Kitty.prototype.render = function(){
 
  window.kittyArray = kittyArray;
  window.Kitty = Kitty;
-});
+ });
