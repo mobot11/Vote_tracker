1ae28b4f042a70a70aa45f6ea146478e3e9cd9d3
Dylan T. Moberg <dylanmoberg@msn.com>
2015-04-16 13:27:32 -0700

    what

---
+++ server.js
@@ -0,0 +1,28 @@
+'use strict';
+
+var express = require('express');
+//grabs it from package.json
+//any time you need to get something from another file, use require & module.export statement
+
+var app = express();
+//middleware added pieces of functionality
+app.use(express.static(__dirname + '/public'));
+
+// app.get('/greet', function(request, response) {
+//   response.status(200).send('<h1>Hello Stranger!</h1>');
+
+  //can only use one .send in our programs
+
+});
+
+// app.get('/*', function (req,res){
+//   res.status(404).send('could not find page');
+// })
+
+// app.get('/greet/:person', function(req,res){
+//   res.send('<h1>Hello '+ req.params.person + '</h1>');
+// })
+
+app.listen(process.env.PORT || 5000, function (){
+  console.log('server running');
+});
