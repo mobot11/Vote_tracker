dedf2e05cba2e46e85d8d49355b512a5683f9f24
Dylan T. Moberg <dylanmoberg@msn.com>
2015-04-16 22:10:33 -0700

    its dead, Jim!

--- public/javascript/kittenChart.js
+++ public/javascript/kittenChart.js
@@ -1,23 +1,8 @@
-var renderTable = function(value1, value2) {
-var kittenTable= document.getElementById("kittenTable").getContext("2d");
-var pieData = [
-    {
-        value: value1,
-        color:"gold"
-    },
-    {
-        value : value2,
-        color : "red"
-    },
-];
-var pieOptions = {
-    segmentShowStroke : false,
-    animateScale : true
-}
-new Chart(kittenTable).Doughnut(pieData, pieOptions);
-};
 
-renderTable(50,50);
+
+
+
+
 
 
 
